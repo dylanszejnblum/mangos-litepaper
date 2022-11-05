df25383ac98196d7f657c0d1ca4a28193911d671
Dylan Szejnblum <dylanszejnblum@gmail.com>
2022-11-05 16:06:22 -0300

    added team section

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import 'nextra-theme-docs/style.css'
+import { MantineProvider } from '@mantine/core'
 
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        /** Put your mantine theme override here */
+        colorScheme: 'light',
+      }}
+    >
+      <Component {...pageProps} />)
+    </MantineProvider>
+  )
 }
