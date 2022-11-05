ff4f842a23164854f090a8383a0d3db1ce30e40c
Dylan Szejnblum <dylanszejnblum@gmail.com>
2022-11-05 14:09:04 -0300

    added timeline component for roadmap

---
+++ pages/data.js
@@ -0,0 +1,16 @@
+export const items = [
+  {
+    title: 'May 1940',
+    cardTitle: 'Dunkirk',
+    url: 'http://www.history.com',
+    cardSubtitle: 'Men of the British Expeditionary Force (BEF) wade out to..',
+    cardDetailedText:
+      'Men of the British Expeditionary Force (BEF) wade out to..',
+    media: {
+      type: 'IMAGE',
+      source: {
+        url: 'http://someurl/image.jpg',
+      },
+    },
+  },
+]
